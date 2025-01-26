5d85d993a4fe914eb496557628e932d3c2050823
whoiskhairul <whoiskhairul@gmail.com>
2025-01-26 02:16:36 +0100

    Update socket base URL to use secure WebSocket protocol in frontend config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 const config = {
   apiBaseUrl: 'https://pure-celebration-production.up.railway.app',
-  socketBaseurl: 'autorack.proxy.rlwy.net:23741'
+  socketBaseurl: 'wss://autorack.proxy.rlwy.net:23741'
 };
 
 export default config;
