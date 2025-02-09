49f829ed07fc743a3725f71d1320c8929e844f84
whoiskhairul <whoiskhairul@gmail.com>
2025-02-09 20:28:55 +0100

    Fix CORS origin by removing trailing slash from production URL

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -254,7 +254,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     'http://localhost:5173',
     "http://localhost:3001",
-    "https://www.folia.sublation.tech/",
+    "https://www.folia.sublation.tech",
 
 ]
 
